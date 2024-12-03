692d6f0893b3718ac1f1a889697ed90e572339c1
Metin100 <9455@holbertonstudents.com>
2024-12-03 20:34:46 +0400

    add third task

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" making float to string """
+
+
+def to_str(n: float) -> str:
+    """
+    to_str float.
+
+    Args:
+        n (float): first float
+
+    Returns:
+        str: string of n.
+    """
+    return str(n)
