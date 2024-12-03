ddd6b9cf9247803595f0af62de08bc4d4c5f4e9b
Metin100 <9455@holbertonstudents.com>
2024-12-03 20:41:02 +0400

    solve fifth task

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" returning sum of input floats """
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    sum_list float.
+
+    Args:
+        input_list (list[float]): list of floats
+
+    Returns:
+        float: sum of input list.
+    """
+    return sum(input_list)
