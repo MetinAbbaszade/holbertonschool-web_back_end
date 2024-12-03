1eda1ec680889ae24efab1b44b00609008640dbd
Metin100 <9455@holbertonstudents.com>
2024-12-03 20:04:13 +0400

    solve second task

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import math
+""" floor float which is n """
+
+
+def floor(n: float) -> int:
+    """
+    Floor float.
+
+    Args:
+        n (float): first float
+
+    Returns:
+        int: floor of n.
+    """
+    return math.floor(n)
