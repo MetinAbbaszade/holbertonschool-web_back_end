514b321b46ff4e65359fb059653ce6d87a370f1f
Metin100 <9455@holbertonstudents.com>
2024-12-03 19:48:57 +0400

    change documentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 def add(a: float, b: float) -> float:
-    """ function to add float """
+    """
+    Adds two float numbers.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of a and b.
+    """
     return a + b
