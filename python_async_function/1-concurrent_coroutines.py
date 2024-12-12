d05f0a04be8605b0456e37818286372d2d1894e0
Metin100 <9455@holbertonstudents.com>
2024-12-12 15:23:54 +0400

    solve first task

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+Test file for printing the correct output of the wait_n coroutine
+'''
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ asyncio function """
+    delays = [wait_random(max_delay) for _ in range(n)]
+    return [await delay for delay in asyncio.as_completed(delays)];
