09d8c4e43bf7b60d0c70c239996fb21b9b5e292c
Metin100 <9455@holbertonstudents.com>
2024-12-12 15:26:27 +0400

    solving error which i thought that it is js:D

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -10,4 +10,4 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ asyncio function """
     delays = [wait_random(max_delay) for _ in range(n)]
-    return [await delay for delay in asyncio.as_completed(delays)];
+    return [await delay for delay in asyncio.as_completed(delays)]
